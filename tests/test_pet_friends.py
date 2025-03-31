77e769fcbc8e8242fd4925d44fb5a51c5c99a3a6
Boris <borman-dallas@yandex.ru>
2025-03-31 09:21:23 +0400

    update file test_pet_friends.py

--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -1,2 +1,14 @@
 from api import PetFriends
 
+from settings import valid_email, valid_password
+
+pf = PetFriends()
+
+def test_get_api_key_for_valid_user(email=valid_email, password=valid_password):
+    status, result = pf.get_api_key(email, password)
+    assert status == 200
+    assert 'key' in result
+
+
+
+
